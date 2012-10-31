f81356eeb5a566d7c5b55d198985685057e1ff5f
Joseph Wecker <jwecker@justin.tv>
2012-10-31 16:58:28 -0700

    Starting to register ff plugins & get them communicating async

--- lib/vim.rb
+++ lib/vim.rb
@@ -1,16 +1,33 @@
+require 'set'
 module Kernel; def require_r(p) require File.join(File.dirname(caller[0]), p.to_str) end end
 require_r '../lib/shorthand.rb'
 
 class VimBuf
   def initialize(pwd, lsline)
     @pwd   = pwd
-    @parts = lsline.strip.scan /^(\d+)([u ])([%# ])([ah ])([-= ])([\+x ]) +"(.*)" +line (\d+)$/
+    @str   = lsline.strip
+    @parts = @str.scan(/^(\d+)([u ])([%# ])([ah ])([-= ])([\+x ]) +"(.*)" +line (\d+)$/)[0]
   end
-  def path()  end
-  def unlisted?() end
-
-
-  #attr_accessor :unlisted, :current, :alternate, :visible, :closed, :modifiable, :readonly, :modified, :errors
+  def inspect() "<VimBuf: #{path} [#{to_set.to_a.join('|')}]>" end
+  def path()        unlisted? ? @parts[6].to_p : @pwd.to_p.join(@parts[6]) end
+  def unlisted?()   @parts[1] == 'u' end
+  def current?()    @parts[2] == '%' end
+  def alternate?()  @parts[2] == '#' end
+  def visible?()    @parts[3] == 'a' end
+  def closed?()     @parts[3] == ' ' end
+  def modifiable?() @parts[4] != '-' && @parts[4] != '=' end
+  def readonly?()   ! modifiable?    end
+  def modified?()   @parts[5] == '+' end
+  def has_errors?() @parts[5] == 'x' end
+  def to_set()
+    return @set unless @set.nil?
+    @set = Set.new
+    @set.add 'unlisted'   if unlisted?   ; @set.add 'current'    if current?
+    @set.add 'alternate'  if alternate?  ; @set.add 'visible'    if visible?
+    @set.add 'closed'     if closed?     ; @set.add 'readonly'   if readonly?
+    @set.add 'modified'   if modified?   ; @set.add 'has_errors' if has_errors?
+    return @set
+  end
 end
 
 class Vim
@@ -44,10 +61,12 @@ class Vim
   def self.cmd(cmd)   Vim.expr("ExCmd(\"#{cmd}\")") end
 
   def self.curr_buffers
-    Vim.cmd('pwd | ls!').each {|out|
+    Vim.cmd('pwd | ls!').map {|out|
       raw  = out.strip.split("\n")
       vpwd = raw.shift
       raw.map{|buff| VimBuf.new(vpwd, buff)}
     }.flatten
   end
 end
+
+
